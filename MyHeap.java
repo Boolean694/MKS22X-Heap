9138e55723f2f6aa9c11c31c9ca68486e2751311
Julian Wong <jwong01@stuy.edu>
2019-04-17 13:58:27 -0400

    methods

---
+++ MyHeap.java
@@ -0,0 +1,17 @@
+public class MyHeap {
+  private static void pushDown(int[] dat, int size, int ind) {
+
+  }
+  private static void pushUp(int[] dat, int size, int ind) {
+
+  }
+  public static void heapify(int[] dat) {
+
+  }
+  public static void heapsort(int[] dat) {
+    
+  }
+  public static void main(String[] args) {
+
+  }
+}
