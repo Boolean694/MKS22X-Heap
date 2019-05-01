4dc5496f02d924fb4ad6010b4baa461cd7068f28
Julian Wong <jwong01@stuy.edu>
2019-05-01 12:10:20 -0400

    i hate java

--- MyHeap.java
+++ MyHeap.java
@@ -6,12 +6,36 @@ public class MyHeap {
 
   }
   public static void heapify(int[] dat) {
-
+    for(int q = 0; q < dat.length; q++) {
+      /*if(dat[q/2] < dat[q]) {
+        int temp = dat[q];
+        dat[q] = dat[q/2];
+        dat[q/2] = temp;
+      }*/
+      if(q * 2 < dat.length) {
+        if(dat[q * 2] > dat[q]) {
+          int temp = dat[q];
+          dat[q] = dat[q * 2];
+          dat[q * 2] = temp;
+        }
+      }
+      if(((q * 2) + 1) < dat.length) {
+        if(dat[(q * 2) + 1] > dat[q]) {
+          int temp = dat[q];
+          dat[q] = dat[(q * 2) + 1];
+          dat[(q * 2) + 1] = temp;
+        }
+      }
+    }
   }
   public static void heapsort(int[] dat) {
-    
+
   }
   public static void main(String[] args) {
-
+    int[] te = {4,8,3,6,2,5,7,9,0,1,3,3,7,4,5,8};
+    heapify(te);
+    for(int q = 0; q < te.length; q++) {
+      System.out.print(te[q] + ",");
+    }
   }
 }
