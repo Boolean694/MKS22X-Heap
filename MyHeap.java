5ba301e7defd7963ea2ab4a364cea16734d2636c
Julian Wong <jwong01@stuy.edu>
2019-05-01 23:33:48 -0400

    start over because alsjlaksjljflasjf

--- MyHeap.java
+++ MyHeap.java
@@ -1,19 +1,21 @@
 public class MyHeap {
   private static void pushDown(int[] dat, int size, int ind) {
-
+    while(ind * 2 + 1 <= size) {//has branches?
+      
+    }
   }
   private static void pushUp(int[] dat, int size, int ind) {
 
   }
   public static void heapify(int[] dat) {
-    
+
   }
   public static void heapsort(int[] dat) {
 
   }
   public static void main(String[] args) {
-    int[] te = {4,8,3,6,2,5,7,8,0,1,3,3,7,9,5,9};
-    heapify(te);
+  int[] te = {4,8,3,6,2,5,7,8,0,1,3,3,7,9/*,5,9,4*/};
+    pushDown(te,te.length - 1,7);
     for(int q = 0; q < te.length; q++) {
       System.out.print(te[q] + ",");
     }
