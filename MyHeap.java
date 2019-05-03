00152160afde298fdc5d960de8de1745493efc7c
Julian Wong <jwong01@stuy.edu>
2019-05-03 00:37:52 -0400

    LITERALLY TWO CHARACTERS WERE AT FAULT FOR MY FRUSTRATION FOR THE PAST 30 MINUTES i hate hate hate hate equals signs

--- MyHeap.java
+++ MyHeap.java
@@ -5,7 +5,7 @@ public class MyHeap {
     }
     else {
       if(ind * 2 + 2 < size) { //two branches
-        if(dat[ind * 2 + 1] < dat[ind] && dat[ind * 2 + 2] < dat[ind]) {return;}//if no more push down, stop
+        if(dat[ind * 2 + 1] <= dat[ind] && dat[ind * 2 + 2] <= dat[ind]) {return;}//if no more push down, stop
         if(dat[ind] < dat[ind * 2 + 1] && dat[ind] < dat[ind * 2 + 2]) {//if both branches larger than node
           if(dat[ind * 2 + 1] >= dat[ind * 2 + 2]) {//if left branch is bigger than right branch, swap node with left, call recursion on new ind
             int temp = dat[ind];
@@ -46,52 +46,21 @@ public class MyHeap {
         return;
       }
     }
-    /*int temp = 0;
-    while(ind * 2 + 1 < size) {//has branches?
-      if(ind * 2 + 2 >= size) {//one branch only
-        if(dat[ind] < dat[ind * 2 + 1]) {//if branch is greater than ind, swap
-          temp = dat[ind];
-          dat[ind] = dat[ind * 2 + 1];
-          dat[ind * 2 + 1] = temp;
-        }
-      }
-      else {//two branches
-        if(Math.min(dat[ind * 2 + 1], dat[ind * 2 + 2]) < dat[ind]) {return;}//if sorted, then stop
-        else {//not sorted
-          if(Math.min(dat[ind * 2 + 1], dat[ind * 2 + 2]) == dat[ind * 2 + 1]) {//if smaller is left branch, swap with right
-
-            System.out.println("two branches, left is smaller, swap with right");
-            System.out.println("dat[ind]: " + dat[ind] + ", right branch: " + dat[ind * 2 + 2]);
-
-            temp = dat[ind];
-            dat[ind] = dat[ind * 2 + 2];
-            dat[ind * 2 + 2] = temp;
-          }
-          else {//if smaller is right branch, swap with left
-
-            System.out.println("two branches, right is smaller, swap with left");
-            System.out.println("dat[ind]: " + dat[ind] + ", left branch: " + dat[ind * 2 + 1]);
-
-            temp = dat[ind];
-            dat[ind] = dat[ind * 2 + 1];
-            dat[ind * 2 + 1] = temp;
-          }
-        }
-      }
-    }*/
   }
   private static void pushUp(int[] dat, int size, int ind) {
 
   }
   public static void heapify(int[] dat) {
-
+    for(int q = dat.length - 1; q >= 0; q--) {
+      pushDown(dat, dat.length, q);
+    }
   }
   public static void heapsort(int[] dat) {
 
   }
   public static void main(String[] args) {
-  int[] te = {5,0,8,7,4,6};
-    pushDown(te,te.length,0);
+  int[] te = {4,8,3,6,2,5,7,9,0,1,3,3,7,4,5,8};
+    heapify(te);
     for(int q = 0; q < te.length; q++) {
       System.out.print(te[q] + ",");
     }
